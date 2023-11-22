eb17e3b2f4e3370d4a86cd952d9df14ba336b332
Ian Wood <ianwood103@gmail.com>
2023-11-22 13:28:12 -0500

    change feed to use current user

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -21,7 +21,7 @@ export function Navbar() {
             <div className="flex flex-row h-24 bg-sage-100 items-center w-full" id="background">
                 <img className="h-24 w-24 rounded-full p-3 ml-2" src="socialcookbooklogo.png"></img>
                 <p className="ml-3 text-4xl">Social Cookbook</p>
-                <a className="text-xl ml-20 hover:bg-sage-200 p-3 rounded-xl" href="/post_board">My Feed</a>
+                <a className="text-xl ml-20 hover:bg-sage-200 p-3 rounded-xl" href="/feed">My Feed</a>
                 <a className="text-xl ml-7 hover:bg-sage-200 p-3 rounded-xl" href="/user">My Cookbook</a>
                 <a className="text-xl ml-7 mr-7 hover:bg-sage-200 p-3 rounded-xl" href="/createpost">Add Recipe</a>
                 <button className="text-lg bg-sage-200 hover:bg-sage-150 p-3 rounded-xl justify-items-end ml-auto mr-5" onClick={Logout}>
